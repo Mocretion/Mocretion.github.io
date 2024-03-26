2fffd47eef0bc095d9a1a6f9a086d779140f0904
Mocretion <danielbundan60@gmail.com>
2024-03-26 13:20:16 +0100

    Added website for fish. Added Seasoned fish

---
+++ codewords.js
@@ -0,0 +1,64 @@
+/**
+ * Replaces codewords in a string with an image
+ * @param {String} string The string to replace the codewords in
+ * @returns A string with /images embeded
+ */
+function replaceCodeWords(string){
+    // Bundles
+    string = string.replace("#BUNDLE_GREEN#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Bundle_Green.png>");
+    string = string.replace("#BUNDLE_ORANGE#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Bundle_Orange.png>");
+    string = string.replace("#BUNDLE_RED#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Bundle_Red.png>");
+    string = string.replace("#BUNDLE_PURPLE#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Bundle_Purple.png>");
+    string = string.replace("#BUNDLE_TEAL#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Bundle_Teal.png>");
+    string = string.replace("#BUNDLE_YELLOW#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Bundle_Yellow.png>");
+    string = string.replace("#BUNDLE_BLUE#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Bundle_Blue.png>");
+
+    // Weather
+    string = string.replace("#RAIN#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Rain.png>");
+    string = string.replace("#WIND#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Windy.png>");
+    string = string.replace("#SUN#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Sunny.png>");
+
+    // Monsters
+    string = string.replace("#SLIME#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Slimes.png>");
+    string = string.replace("#SLIME_BLUE#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Slimes_Blue.png>");
+    string = string.replace("#BAT#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Bat.png>");
+    string = string.replace("#GHOST#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Ghost.png>");
+    string = string.replace("#METAL_HEAD#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Metal_Head.png>");
+    string = string.replace("#SQUID_KID#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Squid_Kid.png>");
+    string = string.replace("#SHADOW_BRUTE#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Shadow_Brute.png>");
+    string = string.replace("#SHADOW_SHAMAN#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Shadow_Shaman.png>");
+
+    // Animals
+    string = string.replace("#CHICKEN#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Brown_Chicken.png>");
+    string = string.replace("#COW#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Brown_Cow.png>");
+    string = string.replace("#GOAT#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Goat.png>");
+    string = string.replace("#DUCK#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Duck.png>");
+    string = string.replace("#SHEEP#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Sheep.png>");
+    string = string.replace("#RABBIT#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Rabbit.png>");
+    string = string.replace("#PIG#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Pig.png>");
+
+    // Characters
+    string = string.replace("#QUEEN#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Cooking_Channel.png>");
+    string = string.replace("#GUS#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Gus_Icon.png>");
+    string = string.replace("#MARNIE#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Marnie_Icon.png>");
+    string = string.replace("#MERCHANT#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Traveling_Cart_Icon.png>");
+
+    // Tools
+    string = string.replace("#SCYTHE#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Scythe.png>");
+    string = string.replace("#HOE#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Hoe.png>");
+    string = string.replace("#TAPPER#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Tapper.png>");
+    string = string.replace("#OIL_MAKER#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Oil_Maker.png>");
+    string = string.replace("#KEG#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Keg.png>");
+    string = string.replace("#PRESERVES_JAR#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Preserves_Jar.png>");
+    string = string.replace("#LOOM#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Loom.png>");
+    string = string.replace("#CHEESE_PRESS#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Cheese_Press.png>");
+    string = string.replace("#FURNACE#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Furnace.png>");
+    string = string.replace("#BEE_HOUSE#", "<img style=\'display:inline-block; height:1em; width:auto; transform:translate(0, 0.1em)\' src=/images/Bee_House.png>");
+
+    // Colors
+    string = string.replace("#RED#", "<span style=\'color: red\'>");
+    string = string.replace('#RED#', "</span>");
+    string = string.replace("#GOLD#", "<span style=\'color: orange\'>");
+    string = string.replace('#GOLD#', "</span>");
+    return string;
+}
--- fish/script.js
+++ fish/script.js
@@ -1,15 +1,12 @@
-let springEnabled = false;
-let summerEnabled = false;
-let fallEnabled = false;
-let winterEnabled = false;
-
 const container = document.getElementById("container");
 
+let springEnabled, summerEnabled, fallEnabled, winterEnabled, sunEnabled = false;
 const springCircle = document.getElementById("spring");
 const summerCircle = document.getElementById("summer");
 const fallCircle = document.getElementById("fall");
 const winterCircle = document.getElementById("winter");
 const winterCircle2 = document.getElementById("winter2");
+const winterCircle3 = document.getElementById("winter3");
 const anySeasonContainer = document.getElementById("any-season-container");
 
 const springCircleText = document.getElementById("springText");
@@ -17,56 +14,37 @@ const summerCircleText = document.getElementById("summerText");
 const fallCircleText = document.getElementById("fallText");
 const winterCircleText = document.getElementById("winterText");
 const winterCircleText2 = document.getElementById("winterText2");
+const winterCircleText3 = document.getElementById("winterText3");
 const anySeasonText = document.getElementById("anySeasonText");
 
 const infoElement = document.getElementById("info");
 const infoLeft = document.getElementById("infoLeft");
 const infoRight = document.getElementById("infoRight");
 
+const sunChidlren = Array.from(document.getElementsByClassName("sun-child"));
 const springChildren = Array.from(document.getElementsByClassName("spring-child"));
 const summerChildren = Array.from(document.getElementsByClassName("summer-child"));
 const fallChildren = Array.from(document.getElementsByClassName("fall-child"));
 const winterChildren = Array.from(document.getElementsByClassName("winter-child"));
 const anySeasonChildren = Array.from(document.getElementsByClassName("anyseason-child"));
-const allItems = new Set(springChildren.concat(summerChildren).concat(fallChildren).concat(winterChildren).concat(anySeasonChildren));
+const allItems = new Set(springChildren.concat(summerChildren).concat(fallChildren).concat(winterChildren).concat(anySeasonChildren).concat(sunChidlren));
 
 const enabledOpacity = 1;
 const disabledOpacity = 0.5;
 const blurDiv = document.getElementById("blur");
 
-const cutQuantityProportion = 4.5;
+const cutQuantityProportion = 3.5;
 
-let dict = {};
-const overwriteDictMax ={
-    "#BUNDLE_PURPLE# Exotic Foraging": 5,
-    "#BUNDLE_TEAL# Quality Crops": 3,
-    "#BUNDLE_PURPLE# Artisan": 6,
-    "#BUNDLE_PURPLE# Crab Pot": 5,
-    "#BUNDLE_RED# Animal": 5,
-    "#BUNDLE_PURPLE# Adventurers": 2,
-    "#BUNDLE_PURPLE# The Missing Bundle": 5
-}
-/**
- * Gets executed on page load
- * Fills "dict" with bundle entries
- */
-function initBundles(){
+function initCookies(){
     let entry;
     allItems.forEach(x => {
-        entry = getItemInfoBundleName(x);
-        if(dict[entry] === undefined){
-            dict[entry] = [x];
-        }else{
-            dict[entry].push(x);
-        }
-
         const cookie = getCookie(getCookieName(x));
         if(cookie == "false") toggleItem(x);
     });
 }
 
 function getCookieName(element){
-    return getItemInfoName(element).replace("\\", "").replace("'", "") + " " + getItemInfoBundleName(element).replace("\\", "").replace("'", "");
+    return "FISH " + getItemInfoName(element).replace("\\", "").replace("'", "");
 }
 
 /**
@@ -123,6 +101,31 @@ function deselectAllCircles(){
     if(summerEnabled) toggleSummer();
     if(fallEnabled) toggleFall();
     if(winterEnabled) toggleWinter();
+    if(sunEnabled) toggleSun();
+}
+
+/**
+ * Toggles focus from the sun circle
+ */
+function toggleSun() {
+
+    if(sunEnabled){
+        setBackgroundAlpha(sunCircle, disabledOpacity);
+        blurDiv.style.opacity = 0;
+        sunCircle.style.zIndex = "auto";
+
+        changeitemsZIndexExcept("sun-child", 80);
+
+    }else{
+        deselectAllCircles();
+        setBackgroundAlpha(sunCircle, enabledOpacity);
+        blurDiv.style.opacity = 0.6;
+        sunCircle.style.zIndex = 50;
+
+        changeitemsZIndexExcept("sun-child", -80);
+    }
+
+    sunEnabled = !sunEnabled;
 }
 
 /**
@@ -201,48 +204,26 @@ function toggleWinter() {
     if(winterEnabled){
         setBackgroundAlpha(winterCircle, disabledOpacity);
         setBackgroundAlpha(winterCircle2, disabledOpacity);
+        setBackgroundAlpha(winterCircle3, disabledOpacity);
         blurDiv.style.opacity = 0;
         winterCircle.style.zIndex = "auto";
         winterCircle2.style.zIndex = "auto";
+        winterCircle3.style.zIndex = "auto";
 
         changeitemsZIndexExcept("winter-child", 80);
     }else{
         deselectAllCircles();
         setBackgroundAlpha(winterCircle, enabledOpacity);
         setBackgroundAlpha(winterCircle2, enabledOpacity);
+        setBackgroundAlpha(winterCircle3, enabledOpacity);
         blurDiv.style.opacity = 0.6;
         winterCircle.style.zIndex = 50;
         winterCircle2.style.zIndex = 50;
+        winterCircle3.style.zIndex = 50;
 
         changeitemsZIndexExcept("winter-child", -80);
     }
     winterEnabled = !winterEnabled;
-}
-
-const blink = [
-    {filter: "brightness(100%", transform: "matrix(1, 0, 0, 1, 0, 0)"},
-    {filter: "brightness(200%", transform: "matrix(1.3, 0, 0, 0.7, 0, 0)"},
-    {filter: "brightness(100%", transform: "matrix(1, 0, 0, 1, 0, 0)"},
-]
-
-const blinkTiming = {
-    duration: 500,
-    iterations: 5
-}
-
-/**
- * Emphasizes items in the same bundle
- * @param event 
- * @param {HTMLElement} element Item in a bundle to get the information from
- */
-function showBundleItems(event, element){
-    
-    getItemsInSameBundle(element).forEach(x => {
-        console.log(x.style.backgroundColor);
-        x.animate(blink, blinkTiming);
-    });
-
-    event.preventDefault();
 }
 
 /**
@@ -266,19 +247,6 @@ function disableAllItems(){
 }
 
 /**
- * Shrinks all items in the bundle
- * @param {HTTPElement} element An item of the bundle
- */
-function disableAllItemInBundle(element){
-    const itemsInBundle = getItemsInSameBundle(element);
-
-    itemsInBundle.forEach(x =>{
-        if(x.style.opacity == 1 || x.style.opacity == "")
-            toggleItem(x);
-    })
-}
-
-/**
  * Shrinks or enlarges the item
  * @param {HTMLElement} element The item to toggle
  */
@@ -291,23 +259,6 @@ function toggleItem(element){
     else{
         element.style.opacity = 0.3;
         element.style.transform = 'scale(0.7)';
-    }
-
-    const index = infoRight.innerHTML.replace("(", "").indexOf("(");
-
-    const completed = getItemsInBundleCompleted(element);
-    const totalNeeded = getItemsNeededInBundle(element);
-
-    let itemsInBundle = completed > totalNeeded ? totalNeeded : completed;
-    infoRight.innerHTML = infoRight.innerHTML.slice(0, index + 2) + itemsInBundle + infoRight.innerHTML.slice(index + 3);
-
-    if(completed >= totalNeeded){
-        infoElement.style.borderColor = "gold";
-
-        if(element.style.opacity == 0.3 && totalNeeded == completed)  // Deactivate other items in bundle if last one got deactivated
-            disableAllItemInBundle(element);
-    }else{
-        infoElement.style.borderColor = "black";
     }
 
     setCookie(getCookieName(element), element.style.opacity == 0.3 ? "false" : "true", 60);
@@ -324,18 +275,15 @@ function showInfo(e, element, ...lines){
     else
         infoElement.style.left = e.clientX - 30 - infoElement.clientWidth;
 
-    if(e.clientY / window.innerHeight > 0.8)  // Show info text on left of mouse
+    if(e.clientY / window.innerHeight > 0.7)  // Show info text on left of mouse
         infoElement.style.top = e.clientY -infoElement.clientHeight;
     else
         infoElement.style.top = e.clientY;
 
     infoElement.style.opacity = 1;
     infoElement.style.visibility = 'visible';
-    if(getItemsInBundleCompleted(element) >= getItemsNeededInBundle(element))
-        infoElement.style.borderColor = "gold";
-    else{
-        infoElement.style.borderColor = "black";
-    }
+    infoElement.style.borderColor = "black";
+    
 }
 
 /**
@@ -367,15 +315,10 @@ function calculateLines(element, lines){
             let i = 0;
             x.forEach(y =>{
                 if(i > 0){
-                    if(j == 0){  // Set Bundle Info
-                        let itemsInBundle = getItemsInBundleCompleted(element) > getItemsNeededInBundle(element) ? getItemsNeededInBundle(element) : getItemsInBundleCompleted(element);
-                        contentRight += "<div>" + replaceCodeWords(y) + " (" + itemsInBundle  + "/" + getItemsNeededInBundle(element) + ") </div>";
-                    }else{  // Set Item Info
-                        if(i == 1){
-                            contentRight += "<div style=\"margin-top:5px\">" + replaceCodeWords(y) + "</div>";
-                        }else{
-                            contentRight += "<div>" + replaceCodeWords(y) + "</div>";
-                        }
+                    if(i == 1){
+                        contentRight += "<div style=\"margin-top:5px\">" + replaceCodeWords(y) + "</div>";
+                    }else{
+                        contentRight += "<div>" + replaceCodeWords(y) + "</div>";
                     }
                     if(i > 1){
                         contentLeft += "<div style=\"opacity:0\"> . </div>";
@@ -392,52 +335,6 @@ function calculateLines(element, lines){
 }
 
 /**
- * @param {HTMLElement} element The item-element in the bundle to get the information from
- * @returns Amount of items completed in bundle
- */
-function getItemsInBundleCompleted(element){
-    let finishedItemsInBundle = 0;
-    for(const [key, value] of Object.entries(dict)){
-        if(key == getItemInfoBundleName(element)){
-            value.forEach(z =>{
-                if(z.style.opacity == 0.3){
-                    finishedItemsInBundle++;
-                }
-            
-            });
-        }
-    }
-
-    return finishedItemsInBundle;
-}
- 
-/**
- * @param {HTMLElement} element The item-element in the bundle to get the information from
- * @returns The amount of items needed to complete a bundle
- */
-function getItemsNeededInBundle(element){
-    const elementKey = getItemInfoBundleName(element);
-    for(const [key, value] of Object.entries(overwriteDictMax)){
-        if(key == elementKey) return value;
-    }
-
-    return dict[elementKey].length;
-}
-
-/**
- * @param {HTMLElement} element The item-element in the bundle to get the information from
- * @returns All item-HTMLElements belonging to a bundle
- */
-function getItemsInSameBundle(element){
-    const elementKey = getItemInfoBundleName(element);
-    for(const [key, value] of Object.entries(dict)){
-        if(key == elementKey) return value;
-    }
-
-    return [];
-}
-
-/**
  * @param {HTMLElement} element item to get the info from
  * @returns item name from their info div
  */
@@ -445,22 +342,15 @@ function getItemInfoName(element){
     return element.onmousemove.toString().trim().split('\'')[1]
 }
 
-/**
- * @param {HTMLElement} element item to get the info from
- * @returns bundle name from their info div
- */
-function getItemInfoBundleName(element){
-    return element.onmousemove.toString().trim().split('\'')[3]
-}
 
 const observer = new ResizeObserver(entries => {
     const width = window.innerWidth;
     const height = window.innerHeight;
 
-    const anySeasonLeft = summerCircle.clientWidth * (1- 1 / cutQuantityProportion) + winterCircle2.clientWidth / 2;
+    const anySeasonLeft = summerCircle.clientWidth * (1- 1 / cutQuantityProportion) + winterCircle2.clientWidth / 2.2;
     anySeasonContainer.style.left = anySeasonLeft;
-    anySeasonContainer.style.width = width * 0.75 - anySeasonLeft;
-    anySeasonContainer.style.height = height * 0.98 - 10;
+    anySeasonContainer.style.width = width * 0.71 - anySeasonLeft;
+    anySeasonContainer.style.height = height * 0.955 - 10;
     anySeasonText.style.left = anySeasonContainer.clientWidth / 2 - anySeasonText.clientWidth / 2;
 
     entries.forEach(entry => {
@@ -484,43 +374,58 @@ const observer = new ResizeObserver(entries => {
                 entry.target.style.left = -entry.target.clientWidth / cutQuantityProportion;
 
                 summerCircleText.style.fontSize = width / 80;
-                border = getCircleBorder(center, center, 60);
+                border = getCircleBorder(center, center, 50);
 
                 summerCircleText.style.left = border[0] - summerCircleText.clientWidth / 2;
                 summerCircleText.style.bottom = border[1] - summerCircleText.clientHeight / 2;
                 break
             case "fall":
-                entry.target.style.left = -entry.target.clientWidth / 2.05;
-                entry.target.style.top = entry.target.clientHeight / 2.3;
+                entry.target.style.left = -entry.target.clientWidth / 1.7;
+                entry.target.style.top = entry.target.clientHeight / 3;
 
                 fallCircleText.style.fontSize = width / 80;
-                border = getCircleBorder(center, center, 290);
+                border = getCircleBorder(center, center, 310);
 
                 fallCircleText.style.left = border[0] - fallCircleText.clientWidth / 2;
                 fallCircleText.style.bottom = border[1] - fallCircleText.clientHeight / 2;
+
+                border = getCircleBorder(center, center, 186);
+                winterCircleText.style.fontSize = width / 80;
+                winterCircleText.style.left = border[0] -winterCircleText.clientWidth / 2;
+                winterCircleText.style.bottom = border[1] - winterCircleText.clientHeight / 2;
                 break;
             case "winter":
-                entry.target.style.width = entry.target.clientHeight / 1.5;
+                entry.target.style.width = entry.target.clientHeight / 3;
                 entry.target.style.left = -entries[0].target.clientWidth / 1.55;
-                winterCircleText.style.fontSize = width / 80;
 
-                border = getCircleBorder(center, center, 230);
-                winterCircleText.style.left = border[0] - winterCircleText.clientWidth / 2;
-                winterCircleText.style.bottom = border[1] - winterCircleText.clientHeight / 1.2;
                 break;
             case "winter2":
                 entry.target.style.width = entry.target.clientHeight;
-                entry.target.style.left = entries[0].target.clientWidth / 2.15;
+                entry.target.style.left = entries[0].target.clientWidth / 3;
                 winterCircleText2.style.fontSize = width / 80;
 
-                border = getCircleBorder(center, center, 50);
+                border = getCircleBorder(center, center, 300);
                 winterCircleText2.style.left = border[0] - winterCircleText2.clientWidth / 2;
                 winterCircleText2.style.bottom = border[1] - winterCircleText2.clientHeight / 1.2;
+                break;
+            case "winter3":
+                entry.target.style.width = entry.target.clientHeight * 2;
+                entry.target.style.left = -entry.target.clientWidth / 1.2;
+
+                winterCircleText3.style.fontSize = width / 80;
+                border = getCircleBorder(center, center, 105);
+
+                winterCircleText3.style.left = entry.target.clientWidth / 1.2 - winterCircleText3.clientWidth / 2;
+                winterCircleText3.style.bottom = border[1] - winterCircleText3.clientHeight / 2;
+
+                entry.target.style.top = -container.clientHeight * 0.05 + winterCircleText3.clientHeight;
+
                 break;
         }
     });
 
-    container.style.left = entries[0].target.clientHeight - entries[0].target.clientHeight / cutQuantityProportion + 30;
+    container.style.left = entries[1].target.clientHeight / 1.2;
+    container.style.top = entries[1].target.clientHeight / 15;
 });
 
 observer.observe(springCircle);
@@ -528,5 +433,6 @@ observer.observe(summerCircle);
 observer.observe(fallCircle);
 observer.observe(winterCircle);
 observer.observe(winterCircle2);
+observer.observe(winterCircle3);
 
-window.onload = initBundles();
+window.onload = initCookies();
